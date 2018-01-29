661f01a957c7ce39d65a3cf2f896ca69ea0c70d7
Ryo Manzoku <manzoku.ryo@gmail.com>
2018-01-29 13:02:47 +0000

    fix

--- config/puma.rb
+++ config/puma.rb
@@ -1,7 +1,8 @@
 directory './'
-threads 8, 32
-workers %x(grep -c processor /proc/cpuinfo)
+threads 4, 4
+workers 2
 bind 'tcp://0.0.0.0:3000'
+activate_control_app 'tcp://127.0.0.1:9293', { auth_token: '12345' }
 pidfile './run/puma.pid'
 stdout_redirect './run/puma_stdout.log', './run/puma_stderr.log', false
-daemonize false
+daemonize true
--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -5,7 +5,7 @@ current_release = release_path
 
 node[:deploy].each do |application, deploy|
   rails_env = deploy[:rails_env] || 'production'
-  ENV = node[:deploy][appname][:environment_variables]
+  ENV = node[:deploy][:appname][:environment_variables]
 
   execute "restart puma" do
     command "sudo su - #{deploy[:user]} -c 'cd /srv/www/[appname]/ && /usr/local/bin/bundle exec puma -C config/puma.rb"
