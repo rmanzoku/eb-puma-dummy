4106d7b29b503d24e2795a10a018f01a7b9f4892
Ryo Manzoku <manzoku.ryo@gmail.com>
2018-01-29 09:49:20 +0000

    Update

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,7 @@
-app do |env|
-  puts env
+directory '/var/app/current'
+bind 'unix:///var/run/puma/my_app.sock'
 
+app do |env|
   body = 'Hello, World!'
 
   [200, { 'Content-Type' => 'text/plain', 'Content-Length' => body.length.to_s }, [body]]
