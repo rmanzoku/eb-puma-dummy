0647de0b656628025459576e8557eb5faa069327
Ryo Manzoku <manzoku.ryo@gmail.com>
2018-01-29 11:22:31 +0000

    Update

--- config.ru
+++ config.ru
@@ -1,5 +1,7 @@
+require 'json'
+
 app = proc do |env|
-  puts "app=ruby-puma-getting-started #{env}"
+  puts JSON.dump(env)
   body = 'Hello, World!'
   [200, { 'Content-Type' => 'text/plain', 'Content-Length' => body.length.to_s }, [body]]
 end
--- config/puma.rb
+++ config/puma.rb
@@ -1,7 +1,7 @@
-directory '/var/app/current'
+directory './'
 threads 8, 32
 workers %x(grep -c processor /proc/cpuinfo)
-bind 'unix:///var/run/puma/my_app.sock'
-pidfile '/var/run/puma/puma.pid'
-stdout_redirect '/var/log/puma/puma.log', '/var/log/puma/puma.log', true
+bind 'tcp://0.0.0.0:3000'
+pidfile './run/puma.pid'
+stdout_redirect './run/puma_stdout.log', './run/puma_stderr.log', false
 daemonize false
