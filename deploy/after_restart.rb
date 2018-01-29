d69bd8516de537e0143264fb8f8d6b8312870ad2
Ryo Manzoku <manzoku.ryo@gmail.com>
2018-01-29 13:07:45 +0000

    fix

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -5,9 +5,9 @@ current_release = release_path
 
 node[:deploy].each do |application, deploy|
   rails_env = deploy[:rails_env] || 'production'
-  ENV = node[:deploy][:appname][:environment_variables]
+  ENV = node[:deploy][appname][:environment_variables]
 
   execute "restart puma" do
-    command "sudo su - #{deploy[:user]} -c 'cd /srv/www/[appname]/ && /usr/local/bin/bundle exec puma -C config/puma.rb"
+    command "sudo su - #{deploy[:user]} -c 'cd /srv/www/sample_rackup/ && /usr/local/bin/bundle exec puma -C config/puma.rb"
   end
 end
