9d79a1c620f0fa474ce8e4cf14d2dc37b362d200
Ryo Manzoku <manzoku.ryo@gmail.com>
2018-01-29 12:58:07 +0000

    Update

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,1 +1,13 @@
 Chef::Log.info("Running deploy/after_restart.rb")
+
+
+current_release = release_path
+
+node[:deploy].each do |application, deploy|
+  rails_env = deploy[:rails_env] || 'production'
+  ENV = node[:deploy][appname][:environment_variables]
+
+  execute "restart puma" do
+    command "sudo su - #{deploy[:user]} -c 'cd /srv/www/[appname]/ && /usr/local/bin/bundle exec puma -C config/puma.rb"
+  end
+end
